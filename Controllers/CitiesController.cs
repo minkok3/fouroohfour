5f9f6442b41ba64dd410865036fb1b23e20b353b
Min Ko <minko.mnd@outlook.com>
2020-02-14 11:31:37 +0000

    oh 404

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FourOohFour.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        private ILogger<CitiesController> _logger;
+
+        private static readonly City[] Cities = new[]
+        {
+            new City {Id = 1, Name = "London"},
+            new City {Id = 2, Name = "Amsterdam"},
+            new City {Id = 3, Name = "Paris"}
+        };
+
+        public CitiesController(ILogger<CitiesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<City> Get()
+        {
+            return Cities;
+        }
+
+        [HttpGet("{id}")]
+        public City GetById(int id)
+        {
+            return Cities.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
